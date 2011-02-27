8933390bdf9c9d12da9062e821b3fc23dc8efad2
Peter Stroiman <peter.s@me.com>
2011-02-27 11:17:33 +0100

    Reformatted DataQueryTestBase

--- DataAccessFramework.UnitTest/DataQueryTestBase.cs
+++ DataAccessFramework.UnitTest/DataQueryTestBase.cs
@@ -6,56 +6,56 @@ using NUnit.Framework;
 
 namespace DataAccessFramework.UnitTest
 {
-	public class DataQueryTestBase
-	{
-		private Mock<DataTool> _dataToolMock = new Mock<DataTool>();
-		protected string ExecutedSql;
-		protected IDataParameter[] ExecutedParameters;
+    public class DataQueryTestBase
+    {
+        private Mock<DataTool> _dataToolMock = new Mock<DataTool>();
+        protected string ExecutedSql;
+        protected IDataParameter[] ExecutedParameters;
 
-		[SetUp]
-		public void Setup()
-		{
-			_dataToolMock = new Mock<DataTool> {CallBase = true};
-			_dataToolMock.Setup(
-				x => x.ExecuteSqlReader(It.IsAny<string>(), It.IsAny<IDataParameter[]>()))
-				.Callback(
-					(string sql, IDataParameter[] parameters) =>
-						{
-							ExecutedSql = sql;
-							ExecutedParameters = parameters;
-						});
-			var createParameterMock = (Func<string, object, IDataParameter>)CreateParameterMock;
-			_dataToolMock.Setup(
-				x => x.CreateIntParameter(It.IsAny<string>(), It.IsAny<int?>()))
-				.Returns(createParameterMock);
-			_dataToolMock.Setup(
-				x => x.CreateStringParameter(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int?>()))
-				.Returns(
-					(string name, object value, int? length) => CreateParameterMock(name, value));
-			_dataToolMock.Setup(
-				x => x.CreateDateTimeParameter(It.IsAny<string>(), It.IsAny<DateTime?>()))
-				.Returns(createParameterMock);
-			_dataToolMock.Setup(
-				x => x.CreateDecimalParameter(It.IsAny<string>(), It.IsAny<decimal?>()))
-				.Returns(createParameterMock);
-		}
+        [SetUp]
+        public void Setup()
+        {
+            _dataToolMock = new Mock<DataTool> { CallBase = true };
+            _dataToolMock.Setup(
+                x => x.ExecuteSqlReader(It.IsAny<string>(), It.IsAny<IDataParameter[]>()))
+                .Callback(
+                    (string sql, IDataParameter[] parameters) =>
+                    {
+                        ExecutedSql = sql;
+                        ExecutedParameters = parameters;
+                    });
+            var createParameterMock = (Func<string, object, IDataParameter>)CreateParameterMock;
+            _dataToolMock.Setup(
+                x => x.CreateIntParameter(It.IsAny<string>(), It.IsAny<int?>()))
+                .Returns(createParameterMock);
+            _dataToolMock.Setup(
+                x => x.CreateStringParameter(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int?>()))
+                .Returns(
+                    (string name, object value, int? length) => CreateParameterMock(name, value));
+            _dataToolMock.Setup(
+                x => x.CreateDateTimeParameter(It.IsAny<string>(), It.IsAny<DateTime?>()))
+                .Returns(createParameterMock);
+            _dataToolMock.Setup(
+                x => x.CreateDecimalParameter(It.IsAny<string>(), It.IsAny<decimal?>()))
+                .Returns(createParameterMock);
+        }
 
-		private static IDataParameter CreateParameterMock(string name, object value)
-		{
-			var parameterMock = new Mock<IDataParameter>();
-			parameterMock.Setup(y => y.ParameterName).Returns(name);
-			parameterMock.Setup(y => y.Value).Returns(value);
-			return parameterMock.Object;
-		}
+        private static IDataParameter CreateParameterMock(string name, object value)
+        {
+            var parameterMock = new Mock<IDataParameter>();
+            parameterMock.Setup(y => y.ParameterName).Returns(name);
+            parameterMock.Setup(y => y.Value).Returns(value);
+            return parameterMock.Object;
+        }
 
-		protected void Execute(Query query)
-		{
-			_dataToolMock.Object.ExecuteQuery(query);
-		}
+        protected void Execute(Query query)
+        {
+            _dataToolMock.Object.ExecuteQuery(query);
+        }
 
-		protected static SelectQuery CreateSelectQuery()
-		{
-			return new SelectQuery();
-		}
-	}
+        protected static SelectQuery CreateSelectQuery()
+        {
+            return new SelectQuery();
+        }
+    }
 }
