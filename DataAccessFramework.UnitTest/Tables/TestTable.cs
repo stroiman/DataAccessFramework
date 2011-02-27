a9024a8991a59c2fc7d6a45a8f508f601a94eb5d
Peter Stroiman <peter.s@me.com>
2011-02-27 11:51:09 +0100

    Created TestTable for testing comparison operator generation

---
+++ DataAccessFramework.UnitTest/Tables/TestTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessFramework.Querying;
+
+namespace DataAccessFramework.UnitTest.Tables
+{
+    public class TestTable : QueryTable
+    {
+        public TestTable() : base("Test")
+        {
+            DateTimeField = Field("DateTimeField");
+        }
+
+        public readonly FieldReference DateTimeField;
+    }
+}
